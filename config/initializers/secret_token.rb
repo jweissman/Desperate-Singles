a4883a8f8ad7a72377b0884027ef9174a585d12f
Mallory <maloret@gmail.com>
2014-07-06 21:33:39 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Despsingles::Application.config.secret_key_base = '97b967f0cec1a925efb7dd2d8c86785f271cf68fbd3d3a33c53c2369fbb7cc8ca3b174bc942c01752d39d595bfc5c50fa3ccb09d1e18f511348e3742743010f2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Despsingles::Application.config.session_store :cookie_store, key: '_despsingles_session'
